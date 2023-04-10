0c99e9442c9c367f53c3f80307c68011a2c8101d
JoaoHong <107969352+JoaoHong@users.noreply.github.com>
2023-04-10 20:46:52 -0300

    Create ola mundo

---
+++ gerenciador/src/main/java/br/com/estacio/gerenciardor/servlet/OlaMundo.java
@@ -0,0 +1,26 @@
+package br.com.estacio.gerenciardor.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns="/oi")
+public class OlaMundo extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("olá mundo, parabens vc escreveu o primeiro servlets.");
+        out.println("</body>");
+        out.println("</html>");
+        
+        System.out.println("o servlet OiMundoServlet foi chamado");
+	}
+}
