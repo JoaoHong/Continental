328d3a3170903cde612a783773ba5e467f578a4e
JoaoHong <107969352+JoaoHong@users.noreply.github.com>
2023-04-11 18:22:43 -0300

    Create ContaBanco.java

---
+++ gerenciador/src/main/java/br/com/estacio/gerenciardor/contabanco/ContaBanco.java
@@ -0,0 +1,16 @@
+package br.com.estacio.gerenciardor.contabanco;
+
+public class ContaBanco {
+	int numConta;
+	String tipo;
+	String dono;
+	float saldo;
+	boolean status;
+	void retorno() {
+		System.out.println("Número: " + this.numConta);
+		System.out.println("Tipo: " + this.tipo);
+		System.out.println("Dono: " + this.dono);
+		System.out.println("Saldo: " + this.saldo + "R$");
+		System.out.println("Status: " + this.status);
+	}
+}
