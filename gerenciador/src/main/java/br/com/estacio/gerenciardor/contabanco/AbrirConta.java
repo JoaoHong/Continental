7a065d4b1f6361395f96f82c4f5030911760c536
Jonathan Soares <104096051+jssoarez@users.noreply.github.com>
2023-04-11 22:10:04 -0300

    Alteração TomCat

--- gerenciador/src/main/java/br/com/estacio/gerenciardor/contabanco/AbrirConta.java
+++ gerenciador/src/main/java/br/com/estacio/gerenciardor/contabanco/AbrirConta.java
@@ -3,6 +3,9 @@ package br.com.estacio.gerenciardor.contabanco;
 
 
 public class AbrirConta{
-	ContaBanco resContaBanco = new ContaBanco();
-	resContaBanco.numConta = 
+	public static void mainAbrirConta(String[] args) {
+		ContaBanco resContaBanco = new ContaBanco();
+		resContaBanco.numConta = 1234;
+		resContaBanco.tipo = "CC";
+	}
 }
