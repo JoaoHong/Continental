66a8ec4e1375d901e130fffa94fe1e61ece7ae3c
JoaoHong <107969352+JoaoHong@users.noreply.github.com>
2023-04-26 21:14:02 -0300

    correção account

--- gerenciador/src/main/java/br/com/estacio/gerenciador/ConexaoBanco/AccountManager.java
+++ gerenciador/src/main/java/br/com/estacio/gerenciador/ConexaoBanco/AccountManager.java
@@ -1,17 +1,19 @@
 package br.com.estacio.gerenciador.ConexaoBanco;
 
 import java.sql.*;
-public class AccountManager {	
-	public static void sacar(String numConta, String valorSaque) {
+
+public class AccountManager {
+	
+	public static void addUsuario(String tipoConta, int numConta, String donoConta, double saldo, boolean status) {
 		String jdbcURL = "jdbc:mysql://continentalbank.cv8y9fpum0xa.us-east-1.rds.amazonaws.com:3306/ContinentalBank";
 		String username = "admin";
 		String password = "3VAJZlkQb3AhrPCvA6jZ";
 		
 		try {
-			Connection connection = DriverManager.getConnection(jdbcURL, username, password);		
+			Connection connection = DriverManager.getConnection(jdbcURL, username, password);
 			
-			String sqlAdd = "INSERT INTO ContaBanco (NumConta, TipoConta, DonoConta, Saldo, Status) VALUES(%s, %s , %s , %s , %s)".formatted(numConta, valorSaque);
-
+			String sqlAdd = "INSERT INTO ContaBanco (NumConta, TipoConta, DonoConta, Saldo, Status) VALUES(%s, %s , %s , %s , %s)".formatted(numConta, tipoConta, donoConta, saldo, status);
+			
 			Statement statement = connection.createStatement();
 			
 			int rows = statement.executeUpdate(sqlAdd);
@@ -25,6 +27,31 @@ public class AccountManager {
 			ex.printStackTrace();
 		}
 	}
+	
+	public static void sacar(int numConta, double saldo) {
+		String jdbcURL = "jdbc:mysql://continentalbank.cv8y9fpum0xa.us-east-1.rds.amazonaws.com:3306/ContinentalBank";
+		String username = "admin";
+		String password = "3VAJZlkQb3AhrPCvA6jZ";
+		
+		try {
+			Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+			
+			String sqlAdd = "INSERT INTO ContaBanco (NumConta, TipoConta, DonoConta, Saldo, Status) VALUES(%s, %s , %s , %s , %s)".formatted(numConta, saldo);
+			
+			Statement statement = connection.createStatement();
+			
+			int rows = statement.executeUpdate(sqlAdd);
+			
+			if (rows > 0) {
+				System.out.println("Nova conta adicionada com sucesso!");
+			}
+			
+			connection.close();
+		}catch(SQLException ex){
+			ex.printStackTrace();
+		}
+	}
+	
 	public static void fecharconta(int numConta) {
 		String jdbcURL = "jdbc:mysql://continentalbank.cv8y9fpum0xa.us-east-1.rds.amazonaws.com:3306/ContinentalBank";
 		String username = "admin";
